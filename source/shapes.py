a0147661a91793053f50521e7920dcca333aad96
Samiksha <gaursamiksha37@gmail.com>
2024-01-28 16:37:59 +0530

    first

---
+++ source/shapes.py
@@ -0,0 +1,20 @@
+class Shape:
+
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi + self.radius ** 2
+
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
